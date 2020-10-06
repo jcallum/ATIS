8bb567cd3904a63c9b93716cd10279632b27e01d
jcallum <john@thecallums.com>
2020-10-06 11:07:15 -0400

    Added template

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+import requests
+import json
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
-    
+@app.route("/atis/")
+@app.route("/atis/<icao>")
+def get_atis(icao = None):
+    return render_template(
+        "get_atis.html",
+        icao = icao
+    )
+
+hdr = {"X-API-Key": "3c5ff1a1547644ffac404bdeba"}
+req = requests.get("https://api.checkwx.com/metar/kjfk/decoded", headers=hdr)
+
+print("Response from CheckWX.... \n")
+
+try:
+    req.raise_for_status()
+    resp = json.loads(req.text)
+    print(json.dumps(resp, indent=1))
+
+except requests.exceptions.HTTPError as e:
+    print(e)
